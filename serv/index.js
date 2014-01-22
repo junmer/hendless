72eb353ed15e86a0eff679975ae3c39472ae1320
junmer <junmer@foxmail.com>
2014-01-22 19:37:18 +0800

    init fe

--- serv/index.js
+++ serv/index.js
@@ -1,20 +1,16 @@
-
 /**
- * Module dependencies.
+ * @file serv
  */
 
-var render = require('./lib/render');
 var logger = require('koa-logger');
 var route = require('koa-route');
 var serve = require('koa-static');
-var views = require('co-views');
-var parse = require('co-body');
 var koa = require('koa');
 var app = koa();
 
 // "database"
 
-var posts = [];
+var db = require('./lib/data');
 
 // middleware
 
@@ -22,56 +18,22 @@ app.use(logger());
 
 // route middleware
 
-app.use(route.get('/', list));
-app.use(route.get('/post/new', add));
-app.use(route.get('/post/:id', show));
-app.use(route.post('/post', create));
+app.use(route.get('/list', list));
+app.use(route.get('/list/:id', list));
+app.use(route.get('/thing/:key', thing));
 
 // serve files from ./public
-app.use(serve(__dirname + '/public'));
-
+app.use(serve(require('path').dirname(__dirname) + '/fe'));
 
 // route definitions
-
-/**
- * Post listing.
- */
-
-function *list() {
-  this.body = yield render('list', { posts: posts });
+function *list(page) {
+    this.body = JSON.stringify(yield db.list(page));
 }
 
-/**
- * Show creation form.
- */
-
-function *add() {
-  this.body = yield render('new');
-}
-
-/**
- * Show post :id.
- */
-
-function *show(id) {
-  var post = posts[id];
-  if (!post) this.throw(404, 'invalid post id');
-  this.body = yield render('show', { post: post });
-}
-
-/**
- * Create a post.
- */
-
-function *create() {
-  var post = yield parse(this);
-  var id = posts.push(post) - 1;
-  post.created_at = new Date;
-  post.id = id;
-  this.redirect('/');
+function *thing(key) {
+    this.body = JSON.stringify(yield db.get(key));
 }
 
 // listen
-
-app.listen(3000);
-console.log('listening on port 3000');
+app.listen(8830);
+console.log('listening on port 8830');
---
+++ serv/lib/data.js
@@ -0,0 +1,80 @@
+/**
+ * @file data
+ */
+
+var fs = require('fs');
+var path = require('path');
+
+var dir = path.resolve(
+    __dirname,
+    '../../crawler/data.json'
+);
+
+var data = {
+    list: [],
+    index: {},
+    pageSize: 5,
+    total: 0,
+    listSize: 0
+};
+
+function get(key) {
+    return data.list[
+        data.index[key]
+    ] || [];
+}
+
+function list(page) {
+
+    var res = {
+        list: [],
+        page: isNaN(+page) ? 0 : +page
+        // total: data.total
+    };
+
+    if ( res.page > data.listSize ) {
+        return res;
+    }
+
+    res.list = data.list.slice(
+        page * data.pageSize, 
+        data.pageSize
+    );
+
+    return res;
+
+}
+
+function createIndex() {
+    data.list.forEach(function (item, index) {
+        data.index[item.key] = index;
+    });
+}
+
+!function init() {
+
+    if( fs.existsSync(dir) ) {
+
+        try {
+            data.list = JSON.parse(
+                fs.readFileSync(dir).toString('utf-8')
+            );
+        }
+        catch (e) {
+            throw 'data init err';
+        }
+        
+        data.total = data.list.length;
+
+        data.listSize = Math.ceil(data.total / data.pageSize);
+
+        createIndex();
+    }
+    else {
+        throw 'data init err';
+    }
+    
+}();
+
+module.exports.get = get;
+module.exports.list = list;
